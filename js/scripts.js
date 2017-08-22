0826fc8b602b0a3572b86b4a14dc81552017d14f
Alfredo Vasquez <vasquez.alf@gmail.com>
2017-08-22 08:42:54 -0500

    Last Update with javascripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
-jQuery(function(){
-    jQuery('ul.menu').superfish();
+jQuery(document).ready(function() {
+	clean(document.body);
+	jQuery(function() {
+		// Dropdown toggle
+		jQuery('.togglebar').click(function(){
+		  jQuery('.responsive-menu').toggle();
+		});
+		jQuery(document).click(function(e) {
+		  var target = e.target;
+		  if (!jQuery(target).is('.togglebar') && !jQuery(target).parents().is('.togglebar')) {
+		    jQuery('.responsive-menu').hide();
+		  }
+		});
+	});
 });
+
+function clean(node)
+{
+  for(var n = 0; n < node.childNodes.length; n ++)
+  {
+    var child = node.childNodes[n];
+    if
+    (
+      child.nodeType === 8 
+      || 
+      (child.nodeType === 3 && !/\S/.test(child.nodeValue))
+    )
+    {
+      node.removeChild(child);
+      n --;
+    }
+    else if(child.nodeType === 1)
+    {
+      clean(child);
+    }
+  }
+}
